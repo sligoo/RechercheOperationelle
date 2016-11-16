d9536fa4d6c6966f9edffa9e510d298b8416f31d
Noah Jay <njay47@gmail.com>
2016-11-16 17:08:46 +0100

    First version of revenus

---
+++ Rendu2/Projet4/revenus.m
@@ -0,0 +1,28 @@
+function [revenu] = revenus(stock, demande, s, S, v, C1, C2, C3, C4)
+% On vend ce que l'on peut
+if stock > demande
+    ventes = demande;
+    penurie = 0;
+else
+    ventes = stock;
+    penurie = demande - stock;
+end
+% Calcul du gain lie aux ventes
+gain_ventes = ventes * v;
+
+% Calcul du cout de stockage
+cout_stockage = stock * C1;
+
+% Calcul du cout de penurie
+cout_penurie = penurie * C2;
+
+% Calcul du cout de commande
+if (stock - demande) < s
+    commande = S - (stock - ventes);
+else
+    commande = 0;
+end
+cout_commande = C3 + commande * C4;
+
+% Revenu de la semaine
+revenu = gain_ventes - cout_stockage - cout_penurie - cout_commande;
