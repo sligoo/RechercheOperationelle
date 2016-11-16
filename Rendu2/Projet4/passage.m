a02637a9f5f737f31dfc9f55c5a131322f4e1dd9
sligoo <sacha.liguori@icloud.com>
2016-11-16 17:20:28 +0100

    ajout de piStar et passage

---
+++ Rendu2/Projet4/passage.m
@@ -0,0 +1,19 @@
+function [ P ] = passage( alpha,s, S )
+% Creation de la matrice de passage (P dans l'?nonc?)
+
+P = zeros(S+1);
+
+P(1:s,1) = sum(alpha(S+1:end)); % [0:s-1,0]
+
+for i = 2:S+1 % [0:s-1,1:end]
+    P(1:s,i) = alpha(S-i+2);
+end
+
+for i = s+1:S+1 
+    P(i,1) = sum(alpha(i:end)); %[s:S,0]
+    for j = 1:i-1
+        P(i,j+1) = alpha(i-j); %[s:S;1:end]
+    end
+end
+end
+
---
+++ Rendu2/Projet4/piStar.m
@@ -0,0 +1,8 @@
+function [ pistar ] = piStar( alpha, s, S)
+%PISTAR Summary of this function goes here
+%   Detailed explanation goes here
+
+pistar 
+
+end
+
