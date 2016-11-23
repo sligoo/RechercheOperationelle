bccd8d41ff1f808348704d8536954872d02625f2
sligoo <sacha.liguori@icloud.com>
2016-11-23 18:44:33 +0100

    modif piStar

--- Rendu2/Projet4/piStar.m
+++ Rendu2/Projet4/piStar.m
@@ -2,7 +2,10 @@ function [ pistar ] = piStar( alpha, s, S)
 %PISTAR Summary of this function goes here
 %   Detailed explanation goes here
 
-pistar 
+P = passage(alpha, s,S);
+P = eye(size(P)) - P ;
+pistar = zeros(S,1);
+
 
 end
 
