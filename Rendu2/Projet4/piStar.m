a6dc91c9dae28c1dbe02ee9f69fbeb6b8a8a40f2
Noah Jay <njay47@gmail.com>
2016-11-23 19:28:21 +0100

    Calcul de Pi*

--- Rendu2/Projet4/piStar.m
+++ Rendu2/Projet4/piStar.m
@@ -2,7 +2,13 @@ function [ pistar ] = piStar( alpha, s, S)
 %PISTAR Summary of this function goes here
 %   Detailed explanation goes here
 
-pistar 
+P = passage(alpha, s, S);
+% A = [I-P <colonne de 1>]
+A = eye(S+1) - P;
+A = [A  ones(S+1, 1)];
+% B = [0, 0, 0, ... , 0, 1] (S+1 zeros)
+B = [zeroes(S+1, 1); 1];
+pistar = A \ B;
 
 end
 
